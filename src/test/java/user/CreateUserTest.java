e7f61a7df542708a957b4d407abd22f5303f9561
Nikolai Zablovsky <nickolas.z@yandex.ru>
2025-08-06 15:49:59 +0300

    fixed after 2 review

--- src/test/java/user/CreateUserTest.java
+++ src/test/java/user/CreateUserTest.java
@@ -1,6 +1,7 @@
 package user;
 
 import io.qameta.allure.junit4.DisplayName;
+import net.datafaker.Faker;
 import org.junit.After;
 import org.junit.Test;
 
@@ -11,15 +12,22 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class CreateUserTest {
 
-    UserAPI userAPI = new UserAPI();
-    User user = new User("Miss@yandex.ru", "Миса", "12345678");
-    User userWoPassword = new User("Miss@yandex.ru", "Миса", "");
-    User userWoEmail = new User("", "Симба", "12345678");
-    User userWoName = new User("Miss@yandex.ru", "", "12345678");
+    private final Faker faker = new Faker();
+    private UserAPI userAPI = new UserAPI();
+    private User user;
+    private User userWoPassword;
+    private User userWoEmail;
+    private User userWoName;
 
     @Test
     @DisplayName("Создание пользователя, успешный запрос")
     public void createUserSuccessfulTest() {
+        user = new User(
+                faker.internet().emailAddress(),
+                faker.name().fullName(),
+                faker.internet().password(8, 12)
+        );
+
         userAPI.createUserRequest(user).then()
                 .assertThat().statusCode(SC_OK)
                 .extract()
@@ -29,6 +37,12 @@ public class CreateUserTest {
     @Test
     @DisplayName("Создание пользователя, который уже зарегистрирован")
     public void createUserDuplicateTest() {
+        user = new User(
+                faker.internet().emailAddress(),
+                faker.name().fullName(),
+                faker.internet().password(8, 12)
+        );
+
         userAPI.createUserRequest(user).then()
                 .assertThat().statusCode(SC_OK);
         userAPI.createUserRequest(user).then()
@@ -40,6 +54,12 @@ public class CreateUserTest {
     @Test
     @DisplayName("Создание пользователя, не заполнен email")
     public void createUserWoEmailTest() {
+        userWoEmail = new User(
+                "",
+                faker.name().fullName(),
+                faker.internet().password(8, 12)
+        );
+
         userAPI.createUserRequest(userWoEmail).then()
                 .assertThat().statusCode(SC_FORBIDDEN)
                 .and()
@@ -49,6 +69,12 @@ public class CreateUserTest {
     @Test
     @DisplayName("Создание пользователя, не заполнен password")
     public void createUserWoPasswordTest() {
+        userWoPassword = new User(
+                faker.internet().emailAddress(),
+                faker.name().fullName(),
+                ""
+        );
+
         userAPI.createUserRequest(userWoPassword).then()
                 .assertThat().statusCode(SC_FORBIDDEN)
                 .and()
@@ -58,6 +84,12 @@ public class CreateUserTest {
     @Test
     @DisplayName("Создание пользователя, не заполнен name")
     public void createUserWoNameTest() {
+        userWoName = new User(
+                faker.internet().emailAddress(),
+                "",
+                faker.internet().password(8, 12)
+        );
+
         userAPI.createUserRequest(userWoName).then()
                 .assertThat().statusCode(SC_FORBIDDEN)
                 .and()
@@ -68,7 +100,7 @@ public class CreateUserTest {
     public void deleteUser() {
         try {
             // Удаляем только успешно созданного пользователя
-            if (user.getEmail() != null && !user.getEmail().isEmpty()
+            if (user != null && user.getEmail() != null && !user.getEmail().isEmpty()
                     && user.getPassword() != null && !user.getPassword().isEmpty()) {
                 String accessToken = userAPI.loginUserRequest(user)
                         .then()
